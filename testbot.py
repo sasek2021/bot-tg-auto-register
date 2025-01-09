b0c25a5a851243c4fd7ccd122df6d07c92816533
sasek2021 <sasek2021@gmail.com>
2025-01-09 12:35:17 +0700

    update test bot

--- testbot.py
+++ testbot.py
@@ -1,24 +1,25 @@
 import os
-from flask import Flask, request
 import telebot
+from flask import Flask, request
 
 app = Flask(__name__)
-
 BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    json_str = request.get_data().decode('UTF-8')
+    json_str = request.get_data(as_text=True)
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
-    return 'OK', 200
+    return '!', 200
 
-@app.route('/')
-def index():
-    return 'Bot is up and running!'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! How are you doing?")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 if __name__ == '__main__':
-    bot.remove_webhook()  # Remove any existing webhook
-    bot.set_webhook(url="https://bot-tg-auto-register.vercel.app/webhook")  # Replace with your Vercel URL
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
